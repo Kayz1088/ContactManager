d4788783d3f9872ad4bf63f77bd0609d78701573
kayz1088 <kennyg8810@gmail.com>
2019-01-21 11:52:11 -0500

    Updates

---
+++ precache-manifest.e4a2e4fe404fea2841ce135ea8f2b787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8c92db63c37f2df9d44",
+    "url": "/static/js/main.b8c92db6.chunk.js"
+  },
+  {
+    "revision": "13f7fc0c54a7e06d5cf5",
+    "url": "/static/js/1.13f7fc0c.chunk.js"
+  },
+  {
+    "revision": "b8c92db63c37f2df9d44",
+    "url": "/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "13f7fc0c54a7e06d5cf5",
+    "url": "/static/css/1.069b3440.chunk.css"
+  },
+  {
+    "revision": "16c2a0317da5e04937a040c05a4edf0b",
+    "url": "/index.html"
+  }
+];
